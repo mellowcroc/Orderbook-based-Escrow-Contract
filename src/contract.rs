526ac6f77b40cd8202d3a3cf658a0b47daae9e40
dylan <mellowcroc@gmail.com>
2021-11-22 22:26:40 +0900

    Fix: `cargo fmt` issues

--- src/contract.rs
+++ src/contract.rs
@@ -1,6 +1,9 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{from_binary, to_binary, Addr, BankMsg, Deps, DepsMut, Env, MessageInfo, Response, StdResult, SubMsg, WasmMsg, Binary};
+use cosmwasm_std::{
+    from_binary, to_binary, Addr, BankMsg, Binary, Deps, DepsMut, Env, MessageInfo, Response,
+    StdResult, SubMsg, WasmMsg,
+};
 use cw2::set_contract_version;
 use cw20::{Balance, Cw20CoinVerified, Cw20ExecuteMsg, Cw20ReceiveMsg};
 
